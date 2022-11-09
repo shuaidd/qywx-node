42999b2582bde9ab2cfd146cba154a6e96fb47b3
ddshuai <ddshuai@shinyway.com.cn>
2022-11-09 14:33:08 +0800

    新增接口

--- src/address-book/index.js
+++ src/address-book/index.js
@@ -1,11 +1,17 @@
 const {user} = require('./user')
 const {dept} = require('./dept')
 const {tag} = require('./tag')
+const {userImport} = require('./import')
+const {userExport} = require('./export')
+const {linkedCorp} = require('./linkedcorp')
 
 exports.addressBook = function (client) {
     return {
         userApi: user(client),
         deptApi: dept(client),
-        tagApi: tag(client)
+        tagApi: tag(client),
+        importApi: userImport(client),
+        exportApi: userExport(client),
+        linkedCorpApi: linkedCorp(client)
     }
 }
